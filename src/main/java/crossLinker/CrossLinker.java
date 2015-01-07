80294754355e38a1de216a255aa6f56d3cf49898
sulezinha <yilmazshule@gmail.com>
2015-01-07 15:02:09 +0100

    JavaDoc for a crossLinker is generated

--- src/main/java/crossLinker/CrossLinker.java
+++ src/main/java/crossLinker/CrossLinker.java
@@ -8,32 +8,45 @@ package crossLinker;
 import com.compomics.util.experiment.biology.ions.ElementaryIon;
 
 /**
+ * This super class hold necessary information for any cross linker agent.
+ *
+ * Note that all mass calculations are based on monoisotopic mass
  *
  * @author Sule
  */
 public abstract class CrossLinker {
 
-    protected CrossLinkerName name;
-    protected double massDift; // monoisotopic or average?- double check this value! all fragment ions are based on average mass, not monoistopic mass
-    protected CrossLinkerType type;
-    //protected int chargeValue;// maybe not necessary?
+    protected CrossLinkerName name; // name
+    protected double massShift_Type2, // when a linker connected to both peptides
+            massShift_Type0; // when a linker only connected to one peptide
+    protected CrossLinkerType type; // either homobifunctional or heterobifunctional
 
-//    public CrossLinker(CrossLinkerName name, double massDift, CrossLinkerType type) {
-//        this.name = name;
-//        this.type = type;
-//        this.massDift = massDift;
-//    }
     /**
-     * This method returns a theoretical mz happens by cross linking 
-     * 
+     * This method returns a theoretical mz happens by cross linking
+     *
      * @param chargeValue ion charge
      * @return
      */
-    public double get_theoretical_mz(int chargeValue) {
-        return (massDift + chargeValue * ElementaryIon.proton.getTheoreticMass()) / chargeValue;
-
+    public double get_theoretical_mz_Type2(int chargeValue) {
+        return (massShift_Type2 + chargeValue * ElementaryIon.proton.getTheoreticMass()) / chargeValue;
     }
 
+    /**
+     * This method returns a theoretical mz happens by cross linking
+     *
+     * @param chargeValue ion charge
+     * @return
+     */
+    public double get_theoretical_mz_Type0(int chargeValue) {
+        return (massShift_Type0 + chargeValue * ElementaryIon.proton.getTheoreticMass()) / chargeValue;
+    }
+
+    /**
+     * Either homobifunctional (links K-K) or heterobifunctional (i.e. EDC with
+     * K either D or E)
+     *
+     * @return an enum type of CrossLinkerType
+     */
     public CrossLinkerType getType() {
         return type;
     }
@@ -42,6 +55,11 @@ public abstract class CrossLinker {
         this.type = type;
     }
 
+    /**
+     * A name of a cross linker agent - DSS, BS3, etc.
+     *
+     * @return an enum type of a CrossLinkerName
+     */
     public CrossLinkerName getName() {
         return name;
     }
@@ -50,12 +68,19 @@ public abstract class CrossLinker {
         this.name = name;
     }
 
-    public double getMassDift() {
-        return massDift;
+    public double getMassShift_Type2() {
+        return massShift_Type2;
     }
 
-    public void setMassDift(double massDift) {
-        this.massDift = massDift;
+    public void setMassShift_Type2(double massShift_Type2) {
+        this.massShift_Type2 = massShift_Type2;
     }
 
+    public double getMassShift_Type0() {
+        return massShift_Type0;
+    }
+
+    public void setMassShift_Type0(double massShift_Type0) {
+        this.massShift_Type0 = massShift_Type0;
+    }
 }
