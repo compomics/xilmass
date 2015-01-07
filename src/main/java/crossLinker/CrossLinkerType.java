3ad424bc020592b3e2bf3f324b4b05f5e1d955d7
sulezinha <yilmazshule@gmail.com>
2015-01-07 15:06:32 +0100

    javadoc for CrossLinkerType is written

--- src/main/java/crossLinker/CrossLinkerType.java
+++ src/main/java/crossLinker/CrossLinkerType.java
@@ -3,14 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package crossLinker;
 
 /**
+ * This enum class hold a cross linker type attribute.
+ *
+ * Either homobifunctional, so both ends of a linker agent are specific to the
+ * same residue. Like DSS-K and K.
+ *
+ * Or hetereobifunctional, ends of a linker agents are specific to different
+ * residues. Like EDC-K and D/E.
  *
  * @author Sule
  */
 public enum CrossLinkerType {
     homobifunctional,
-    heterobifunctional    
+    heterobifunctional
 }
