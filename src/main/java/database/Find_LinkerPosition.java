1591d5fb3f8b7a28ee99390e445874fc8e685da0
sulezinha <yilmazshule@gmail.com>
2015-01-26 15:08:56 +0100

    find_possibly_linker_locations method is modified for both hetero- and homobifunctionality

--- src/main/java/database/Find_LinkerPosition.java
+++ src/main/java/database/Find_LinkerPosition.java
@@ -5,7 +5,7 @@
  */
 package database;
 
-import crossLinker.CrossLinkerName;
+import crossLinker.CrossLinker;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -15,40 +15,53 @@ import java.util.HashMap;
  */
 public class Find_LinkerPosition {
 
-    public static HashMap<String, ArrayList<Integer>> find_possibly_linker_locations(String startSequence, CrossLinkerName crossLinker) {
+    public static HashMap<String, ArrayList<Integer>> find_possibly_linker_locations(String startSequence, CrossLinker crossLinker) {
         HashMap<String, ArrayList<Integer>> crossLinker_and_indices = new HashMap<String, ArrayList<Integer>>();
-        switch (crossLinker) {
-            case DSS:
-                // k-k
-                ArrayList<Integer> indices = new ArrayList<Integer>();
-                for (int i = 0; i < startSequence.length(); i++) {
-                    char charAt = startSequence.charAt(i);
-                    if (charAt == 'K' && i != startSequence.length() - 1) {
-                        indices.add(i);
-                    }
-                    crossLinker_and_indices.put("K", indices);
-                }
+        switch (crossLinker.getName()) {
+            case BS3:
+                link_homobifunctional(startSequence, crossLinker_and_indices);
                 break;
-
+            case DSS:
+                link_homobifunctional(startSequence, crossLinker_and_indices);
+                break;
+            case GA:
+                link_homobifunctional(startSequence, crossLinker_and_indices);
+                break;
             case EDC:
-                ArrayList<Integer> indices_K = new ArrayList<Integer>(),
-                 indices_E = new ArrayList<Integer>(),
-                 indices_D = new ArrayList<Integer>();
-                for (int i = 0; i < startSequence.length(); i++) {
-                    char charAt = startSequence.charAt(i);
-                    if (charAt == 'K' && i != startSequence.length() - 1) {
-                        indices_K.add(i);
-                    } else if (charAt == 'E' && i != startSequence.length() - 1) {
-                        indices_E.add(i);
-                    } else if (charAt == 'D' && i != startSequence.length() - 1) {
-                        indices_D.add(i);
-                    }
-                }
-                crossLinker_and_indices.put("K", indices_K);
-                crossLinker_and_indices.put("D", indices_D);
-                crossLinker_and_indices.put("E", indices_E);
+                link_heterobifunctional(startSequence, crossLinker_and_indices);
                 break;
         }
         return crossLinker_and_indices;
+    }
+
+    private static void link_heterobifunctional(String startSequence, HashMap<String, ArrayList<Integer>> crossLinker_and_indices) {
+        ArrayList<Integer> indices_K = new ArrayList<Integer>(),
+                indices_E = new ArrayList<Integer>(),
+                indices_D = new ArrayList<Integer>();
+        for (int i = 0; i < startSequence.length(); i++) {
+            char charAt = startSequence.charAt(i);
+            if (charAt == 'K' && i != startSequence.length() - 1) {
+                indices_K.add(i);
+            } else if (charAt == 'E' && i != startSequence.length() - 1) {
+                indices_E.add(i);
+            } else if (charAt == 'D' && i != startSequence.length() - 1) {
+                indices_D.add(i);
+            }
+        }
+        crossLinker_and_indices.put("K", indices_K);
+        crossLinker_and_indices.put("D", indices_D);
+        crossLinker_and_indices.put("E", indices_E);
+    }
+
+    private static void link_homobifunctional(String startSequence, HashMap<String, ArrayList<Integer>> crossLinker_and_indices) {
+        // k-k
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        for (int i = 0; i < startSequence.length(); i++) {
+            char charAt = startSequence.charAt(i);
+            if (charAt == 'K' && i != startSequence.length() - 1) {
+                indices.add(i);
+            }
+            crossLinker_and_indices.put("K", indices);
+        }
     }
 }
