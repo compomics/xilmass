a6f4109add22527d7c16762895740e64aafa7ed7
sulezinha <yilmazshule@gmail.com>
2015-01-28 15:43:06 +0100

    heavy label case with replacing H to Deuterium

--- src/main/java/crossLinker/type/BS3.java
+++ src/main/java/crossLinker/type/BS3.java
@@ -18,10 +18,28 @@ public class BS3 extends CrossLinker {
 
     public BS3() {
         this.name = CrossLinkerName.BS3;
-        this.type = CrossLinkerType.homobifunctional; // K-K
-        double moleculeMass = (8*Atom.C.getMonoisotopicMass())+ (2*Atom.O.getMonoisotopicMass())+(12*Atom.H.getMonoisotopicMass());
+        this.type = CrossLinkerType.homobifunctional; // K-K     
+        double moleculeMass = (8 * Atom.C.getMonoisotopicMass()) + (2 * Atom.O.getMonoisotopicMass()) + (12 * Atom.H.getMonoisotopicMass());
         super.massShift_Type0 = moleculeMass - (Atom.O.getMonoisotopicMass());
-        super.massShift_Type2 = moleculeMass - (2*Atom.H.getMonoisotopicMass());
+        super.massShift_Type2 = moleculeMass - (2 * Atom.H.getMonoisotopicMass());
+    }
+
+    // To introduce heavy options, otherwise it is "light only"
+    // Here deteurium is used instead of hydrogen atom
+    public BS3(boolean isLabeled) {
+        this.name = CrossLinkerName.BS3;
+        this.type = CrossLinkerType.homobifunctional; // K-K     
+        double hydrogen_mass = Atom.H.getMonoisotopicMass(),
+                deuterium_mass = Atom.H.getIsotopeMass(1);
+        if (isLabeled) {
+            double moleculeMass = (8 * Atom.C.getMonoisotopicMass()) + (2 * Atom.O.getMonoisotopicMass()) + (12 * deuterium_mass);
+            super.massShift_Type0 = moleculeMass - (Atom.O.getMonoisotopicMass());
+            super.massShift_Type2 = moleculeMass - (2 * deuterium_mass);
+        } else {
+            double moleculeMass = (8 * Atom.C.getMonoisotopicMass()) + (2 * Atom.O.getMonoisotopicMass()) + (12 * hydrogen_mass);
+            super.massShift_Type0 = moleculeMass - (Atom.O.getMonoisotopicMass());
+            super.massShift_Type2 = moleculeMass - (2 * hydrogen_mass);
+        }
     }
 
 }
