761e01f84d456250290ec6e6151c08f0d92d39c4
sulezinha <yilmazshule@gmail.com>
2015-04-22 22:47:40 +0200

    IonType is renamed to CPeptideIon to prevent confusion.
    CPeptideIon is improved. Not there are Linker/BackBone and LinkedPeptide are avaliable for PepA and PepB

---
+++ src/main/java/theoretical/CPeptideIonType.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package theoretical;
+
+/**
+ * It shows which ion type is on a crossLinkedPeptide
+ 
+ Example: RLGK*PGS|AI*KN
+ 
+ PeptideFragmentIons - RLGKPGS and RGWAKAM 
+ Linker - DSS/EDC orBS3..
+ * LinkedPeptideBackbone_PepA - AI*KN would yield ions of cK, IK, AIK, KN, IKN, AIKN
+ * 
+ * @author Sule
+ */
+public enum CPeptideIonType {    
+    Backbone_PepA,
+    Backbone_PepB,
+    Linker,
+    LinkedPeptideBackbone_PepA,
+    LinkedPeptideBackbone_PepB
+}
