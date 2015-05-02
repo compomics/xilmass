081629d697470393da843e594ddeab71d843174c
sulezinha <yilmazshule@gmail.com>
2015-05-02 16:14:05 +0200

    getFixtPTMTest is improved

--- src/test/java/start/GetFixedPTMTest.java
+++ src/test/java/start/GetFixedPTMTest.java
@@ -7,15 +7,16 @@ package start;
 
 import com.compomics.util.experiment.biology.PTM;
 import com.compomics.util.experiment.biology.PTMFactory;
+import com.compomics.util.experiment.biology.Peptide;
 import com.compomics.util.experiment.identification.matches.ModificationMatch;
 import java.io.File;
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -44,13 +45,37 @@ public class GetFixedPTMTest {
 
     /**
      * This simple getPTM method runs to check one example case.
-     * 
+     *
      * Test of getPTM method, of class GetFixedPTM.
      */
     @Test
     public void testGetPTM() throws Exception {
         System.out.println("getPTM");
         String peptideSequence = "MLCSDA";
+        // Importing PTMs
+        File modsFile = new File("C:\\Users\\Sule\\Documents\\NetBeansProjects\\compomics-utilities\\src/test/resources/experiment/mods.xml");
+        PTMFactory ptmFactory = PTMFactory.getInstance();
+        ptmFactory.importModifications(modsFile, false);
+        // Getting one fixed PTMs
+        PTM ptmName = ptmFactory.getPTM("carbamidomethyl c");
+        ArrayList<String> theoreticPTMs = new ArrayList<String>();
+        theoreticPTMs.add("carbamidomethyl c");
+        String theoreticPTM = ptmName.getName();
+        ArrayList<ModificationMatch> result = GetFixedPTM.getPTM(ptmFactory, theoreticPTMs, peptideSequence);
+        for (ModificationMatch acMatch : result) {
+            assertEquals(theoreticPTM, acMatch.getTheoreticPtm());
+            assertEquals(3, acMatch.getModificationSite());
+        }
+        assertEquals(1, result.size());
+        Peptide peptideObject = new Peptide(peptideSequence, result);
+        assertEquals("MLcSDA", peptideObject.getSequenceWithLowerCasePtms());
+
+    }
+
+    @Test
+    public void testGetPTMAlt() throws Exception {
+        System.out.println("getPTM");
+        String peptideSequence = "MLCSDAOP";
 
         // Importing PTMs
         File modsFile = new File("C:\\Users\\Sule\\Documents\\NetBeansProjects\\compomics-utilities\\src/test/resources/experiment/mods.xml");
@@ -58,14 +83,14 @@ public class GetFixedPTMTest {
         ptmFactory.importModifications(modsFile, false);
 
         // Getting one fixed PTMs
-        PTM ptmName = ptmFactory.getPTM("carbamidomethyl c");
-        String theoreticPTM = ptmName.getName();
-        ArrayList<ModificationMatch> result = GetFixedPTM.getPTM(ptmFactory, theoreticPTM, peptideSequence);
-        for (ModificationMatch acMatch : result) {
-            assertEquals(theoreticPTM, acMatch.getTheoreticPtm());
-            assertEquals(3, acMatch.getModificationSite());
-        }
-        assertEquals(1, result.size());
+        ArrayList<String> theoreticPTMs = new ArrayList<String>();
+        theoreticPTMs.add("oxidation of m");
+        theoreticPTMs.add("pyro-cmc");
+        ArrayList<ModificationMatch> result = GetFixedPTM.getPTM(ptmFactory, theoreticPTMs, peptideSequence);
+        Peptide peptideObject = new Peptide(peptideSequence, result);
+        assertEquals("mLcSDAOP", peptideObject.getSequenceWithLowerCasePtms());
+        assertEquals(2, result.size());
+
     }
 
 }
