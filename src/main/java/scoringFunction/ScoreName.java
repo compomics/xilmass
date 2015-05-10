58d51cac9437788c796f50ecf949059b1bc126b5
sulezinha <yilmazshule@gmail.com>
2015-05-10 14:46:22 +0200

    naming has changed and added information next to them.

--- src/main/java/scoringFunction/ScoreName.java
+++ src/main/java/scoringFunction/ScoreName.java
@@ -7,11 +7,11 @@
 package scoringFunction;
 
 /**
- *
+ * Scoring function names
  * @author Sule
  */
 public enum ScoreName {
-    Andromeda,
-    MSAmanda,
-    TheoMSAmanda
+    AndromedaD, // Andromeda derive - cumulative binomial probability derived function/only number of peak/N=theoretical peaks 
+    MSAmandaD, // MSAmanda derive - cumulative binomial probability derived function/number of peak and explained intensities/N=matched experimental peaks 
+    TheoMSAmandaD // (Theoretical)MSAmanda derive - cumulative binomial probability derived function/number of peak and explained intensities/N=theoretical experimental peaks 
 }
