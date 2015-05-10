3a8aa4acd9ae3f217af7e5c96bbb516c33a577aa
sulezinha <yilmazshule@gmail.com>
2015-05-10 14:47:51 +0200

    a score package was added to be able doing multithreading while spectrum-crosslinked peptide calculation

---
+++ src/main/java/multithread/score/Result.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithread.score;
+
+import com.compomics.util.experiment.massspectrometry.MSnSpectrum;
+import com.compomics.util.experiment.massspectrometry.Peak;
+import java.util.HashSet;
+import scoringFunction.ScoreName;
+import theoretical.CPeptidePeak;
+import theoretical.CPeptides;
+
+/**
+ * This method keeps results spectrum-cross linked peptide score calculation via
+ * multi-threading
+ *
+ * @author Sule
+ */
+public class Result {
+
+    private MSnSpectrum msms; // a selected MSnSpectrum object
+    private CPeptides cp; // a cross linked peptide object
+    private double score; // score how likely that cross linked peptide spectra looks like MSnSpectrum object
+    private ScoreName scoreName; // a name of scoring function
+    private HashSet<Peak> matchedPeaks; // list of matched peaks on an experimental spectrum 
+    private HashSet<CPeptidePeak> matchedCTheoPeaks; // list of theoretical peaks matched on a theoretical spectrum
+
+    /**
+     *
+     * @param msms MSnSpectrum object
+     * @param cp CPeptides
+     * @param scoreName which scoring function
+     * @param score calculated score
+     * @param matchedPeaks matched experimental peaks
+     * @param matchedCTheoPeaks matched theoretical peaks
+     */
+    public Result(MSnSpectrum msms, CPeptides cp, ScoreName scoreName, double score, HashSet<Peak> matchedPeaks, HashSet<CPeptidePeak> matchedCTheoPeaks) {
+        this.msms = msms;
+        this.cp = cp;
+        this.score = score;
+        this.scoreName = scoreName;
+        this.matchedCTheoPeaks = matchedCTheoPeaks;
+        this.matchedPeaks = matchedPeaks;
+    }
+
+    /* Getter and setter method for Result information */
+    public MSnSpectrum getMsms() {
+        return msms;
+    }
+
+    public void setMsms(MSnSpectrum msms) {
+        this.msms = msms;
+    }
+
+    public CPeptides getCp() {
+        return cp;
+    }
+
+    public void setCp(CPeptides cp) {
+        this.cp = cp;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public ScoreName getScoreName() {
+        return scoreName;
+    }
+
+    public void setScoreName(ScoreName scoreName) {
+        this.scoreName = scoreName;
+    }
+
+    public HashSet<Peak> getMatchedPeaks() {
+        return matchedPeaks;
+    }
+
+    public void setMatchedPeaks(HashSet<Peak> matchedPeaks) {
+        this.matchedPeaks = matchedPeaks;
+    }
+
+    public HashSet<CPeptidePeak> getMatchedCTheoPeaks() {
+        return matchedCTheoPeaks;
+    }
+
+    public void setMatchedCTheoPeaks(HashSet<CPeptidePeak> matchedCTheoPeaks) {
+        this.matchedCTheoPeaks = matchedCTheoPeaks;
+    }
+
+}
---
+++ src/main/java/multithread/score/Score.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithread.score;
+
+import multithread.score.Result;
+import com.compomics.util.experiment.biology.PTMFactory;
+import com.compomics.util.experiment.biology.Peptide;
+import com.compomics.util.experiment.identification.matches.ModificationMatch;
+import com.compomics.util.experiment.massspectrometry.MSnSpectrum;
+import com.compomics.util.experiment.massspectrometry.Peak;
+import crossLinker.CrossLinker;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.concurrent.Callable;
+import matching.MatchAndScore;
+import org.xmlpull.v1.XmlPullParserException;
+import scoringFunction.ScoreName;
+import start.GetPTMs;
+import theoretical.CPeptidePeak;
+import theoretical.CPeptides;
+import theoretical.FragmentationMode;
+
+/**
+ *
+ * @author Sule
+ */
+public class Score implements Callable<ArrayList<Result>> {
+
+    private ArrayList<MSnSpectrum> selectedSpectra; // all selected CPeptides objects
+    private CPeptides cPeptide; // a MSnSpectrum compared to CPeptides object
+    private ScoreName scoreName; // a ScoreName
+    private double fragTol, // fragment tolerance, requiring for MatchAndScore instantiation.
+            massWindow; // mass window, requiring for MatchAndScore instantiation.
+    private int intensityOptionForMSAmanda, // fragment tolerance, requiring for MatchAndScore instantiation.
+            minFilteredPeakNumber, // min number of filtered peak per window,, requiring for MatchAndScore instantiation.
+            maxFilteredPeakNumber; // max number of filtered peak per window, requiring for MatchAndScore instantiation.
+    private String line;
+    private CrossLinker linker;
+    private FragmentationMode fragMode;
+    private PTMFactory ptmFactory;
+    private boolean isBranching;
+    
+    // A constructor for multithreading
+    public Score(ArrayList<MSnSpectrum> selectedSpectra, String line, ScoreName scoreName, PTMFactory ptmFactory, CrossLinker linker, FragmentationMode fragMode,
+            double fragTol, int intensityOptionForMSAmanda, int minFilteredPeakNumber, int maxFilteredPeakNumber, double massWindow, boolean isBranching) {
+        this.selectedSpectra = selectedSpectra;
+        this.line = line;
+        this.scoreName = scoreName;
+        this.ptmFactory = ptmFactory;
+        this.linker = linker;
+        this.fragMode = fragMode;
+        this.fragTol = fragTol;
+        this.intensityOptionForMSAmanda = intensityOptionForMSAmanda;
+        this.minFilteredPeakNumber = minFilteredPeakNumber;
+        this.maxFilteredPeakNumber = maxFilteredPeakNumber;
+        this.massWindow = massWindow;
+        this.isBranching = isBranching;
+    }
+
+    /**
+     * A call method for multithreading. It calculates score for a given
+     * MSnSpectrum against all CPeptides object
+     *
+     * @return a list of Result object containing MSMS/CPeptides/ScoreName/Score
+     * @throws Exception
+     */
+    @Override
+    public ArrayList<Result> call() throws Exception {
+        cPeptide = getCPeptides();
+        ArrayList<Result> results = new ArrayList<Result>();
+        InnerIteratorSync<MSnSpectrum> iteratorCPeptides = new InnerIteratorSync(selectedSpectra.iterator());
+        while (iteratorCPeptides.iter.hasNext()) {
+            MSnSpectrum tmpMSMS = (MSnSpectrum) iteratorCPeptides.iter.next();
+            synchronized (tmpMSMS) {
+                // First generate CPeptides object.  
+                MatchAndScore obj = new MatchAndScore(tmpMSMS, scoreName, cPeptide, fragTol, intensityOptionForMSAmanda, minFilteredPeakNumber, maxFilteredPeakNumber, massWindow);
+                double tmpScore = obj.getCXPSMScore();
+                HashSet<Peak> matchedPeaks = obj.getMatchedPeaks();
+                HashSet<CPeptidePeak> matchedTheoreticalCPeaks = obj.getMatchedTheoreticalCPeaks();
+                Result r = new Result(tmpMSMS, cPeptide, scoreName, tmpScore, matchedPeaks, matchedTheoreticalCPeaks);
+                results.add(r);
+            }
+        }
+        return results;
+    }
+
+    private CPeptides getCPeptides() throws XmlPullParserException, IOException {
+        String[] split = line.split("\t");
+        String proteinA = split[0],
+                proteinB = split[1],
+                peptideAseqFile = split[2],
+                peptideBseqFile = split[3],
+                fixedModA = split[6],
+                fixedModB = split[7],
+                variableModA = split[8],
+                variableModB = split[9];
+        // linker positions...
+        Integer linkerPosPeptideA = Integer.parseInt(split[4]),
+                linkerPosPeptideB = Integer.parseInt(split[5]);
+        ArrayList<ModificationMatch> fixedPTM_peptideA = GetPTMs.getPTM(ptmFactory, fixedModA, false),
+                fixedPTM_peptideB = GetPTMs.getPTM(ptmFactory, fixedModB, false);
+        // Start putting them on a list which will contain also variable PTMs
+        ArrayList<ModificationMatch> ptms_peptideA = new ArrayList<ModificationMatch>(fixedPTM_peptideA),
+                ptms_peptideB = new ArrayList<ModificationMatch>(fixedPTM_peptideB);
+        // Add variable PTMs and also a list of several fixed PTMs
+        ArrayList<ModificationMatch> variablePTM_peptideA = GetPTMs.getPTM(ptmFactory, variableModA, true),
+                variablePTM_peptideB = GetPTMs.getPTM(ptmFactory, variableModB, true);
+        ptms_peptideA.addAll(variablePTM_peptideA);
+        ptms_peptideB.addAll(variablePTM_peptideB);
+        // First peptideA
+        Peptide peptideA = new Peptide(peptideAseqFile, ptms_peptideA),
+                peptideB = new Peptide(peptideBseqFile, ptms_peptideB);
+        // now generate peptide...
+        CPeptides tmpCpeptide = new CPeptides(proteinA, proteinB, peptideA, peptideB, linker, linkerPosPeptideA, linkerPosPeptideB, fragMode, isBranching);
+        return tmpCpeptide;
+    }
+
+    /**
+     * Simple wrapper class to allow synchronisation on the hasNext() and next()
+     * methods of the iterator.
+     */
+    private class InnerIteratorSync<T> {
+
+        private Iterator<T> iter = null;
+
+        public InnerIteratorSync(Iterator<T> aIterator) {
+            iter = aIterator;
+        }
+
+        public synchronized T next() {
+            T result = null;
+            if (iter.hasNext()) {
+                result = iter.next();
+            }
+            return result;
+        }
+    }
+}
