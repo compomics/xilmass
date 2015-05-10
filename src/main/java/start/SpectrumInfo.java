291dca1e02b00e1e2dd1e002153a78db36304959
sulezinha <yilmazshule@gmail.com>
2015-05-10 14:45:37 +0200

    SpectrumInfo class is added to enable sorting of all stored spectra

---
+++ src/main/java/start/SpectrumInfo.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package start;
+
+import com.compomics.util.experiment.massspectrometry.MSnSpectrum;
+import java.util.Comparator;
+
+/**
+ * This class is used to sort an arraylist of MSnSpectrum objects based on their
+ * precursor masses
+ *
+ * @author Sule
+ */
+public class SpectrumInfo {
+
+    private MSnSpectrum ms;  // a MSnSpectrum.
+    private double actualPrecMass; // an actual precursor mass from a selected MS2 spectrum
+
+    /**
+     * To construct SpectrumInfo object
+     * 
+     * @param ms is MSnSpectrum object
+     * @param actualPrecMass  a precursor mass of that MSnSpectrum object
+     */
+    public SpectrumInfo(MSnSpectrum ms, double actualPrecMass) {
+        this.ms = ms;
+        this.actualPrecMass = actualPrecMass;
+    }
+
+    /**
+     * To return MSnSpectrum object
+     * 
+     * @return 
+     */
+    public MSnSpectrum getMS() {
+        return ms;
+    }
+
+    /**
+     * To set MSnSpectrum object to another one.
+     * It also calculates again precursor mass..
+     *
+     * @param ms
+     */
+    public void setMSnSpectrum(MSnSpectrum ms) {
+        this.ms = ms;
+        actualPrecMass = CalculatePrecursorMass.getPrecursorMass(ms);
+    }
+
+    /**
+     * This method return a precursor mass from a given MSnSpectrum object
+     * 
+     * @return 
+     */
+    public double getPrecursorMass() {
+        return actualPrecMass;
+    }
+
+    
+    /**
+     * To sort SpectrumInfo objects in a ascending m/z order
+     */
+    public static final Comparator<SpectrumInfo> Precursor_ASC_mz_order
+            = new Comparator<SpectrumInfo>() {
+                @Override
+                public int compare(SpectrumInfo o1, SpectrumInfo o2) {
+                    return o1.getPrecursorMass() < o2.getPrecursorMass() ? -1 : o1.getPrecursorMass() == o2.getPrecursorMass() ? 0 : 1;
+                }
+            };
+
+}
