5417330e949c83ac0e5eee119183bf1738642c01
sulezinha <yilmazshule@gmail.com>
2015-05-17 19:13:08 +0200

    naming is improved with single/node

--- src/main/java/naming/DefineIdCPeptideFragmentationPattern.java
+++ src/main/java/naming/DefineIdCPeptideFragmentationPattern.java
@@ -45,6 +45,7 @@ public class DefineIdCPeptideFragmentationPattern {
         this.linkerPositionOnPepB = linkerPositionOnPepB;
         this.pepALen = pepALen;
         this.pepBLen = pepBLen;
+//        name();
     }
 
     /**
@@ -74,11 +75,15 @@ public class DefineIdCPeptideFragmentationPattern {
      *
      */
     private void name() {
-        for (CPeptidePeak tmpCPPeak : matchedCPepPeaks) {
-            String tmpPeakName = tmpCPPeak.getName();
-            checkStatus(tmpPeakName);
+        if (matchedCPepPeaks.size() == 1) {
+            name = IdCPeptideFragmentationPatternName.SINGLE;
+        } else {
+            for (CPeptidePeak tmpCPPeak : matchedCPepPeaks) {
+                String tmpPeakName = tmpCPPeak.getName();
+                checkStatus(tmpPeakName);
+            }
+            defineStatus();
         }
-        defineStatus();
     }
 
     @Override
@@ -136,6 +141,10 @@ public class DefineIdCPeptideFragmentationPattern {
             name = IdCPeptideFragmentationPatternName.ALLOVER;
         } else if (ions_PepA_Right == 0 && ions_PepB_Right == 0 && ions_PepA_Left == 0 && ions_PepB_Left == 0 && ions_PepA_Node > 0 && ions_PepB_Node > 0) {
             name = IdCPeptideFragmentationPatternName.LINK;
+        } else if (ions_PepA_Right == 0 && ions_PepB_Right == 0 && ions_PepA_Left == 0 && ions_PepB_Left == 0 && ions_PepA_Node > 0 && ions_PepB_Node == 0) {
+            name = IdCPeptideFragmentationPatternName.NODE_PEPA;
+        } else if (ions_PepA_Right == 0 && ions_PepB_Right == 0 && ions_PepA_Left == 0 && ions_PepB_Left == 0 && ions_PepA_Node == 0 && ions_PepB_Node > 0) {
+            name = IdCPeptideFragmentationPatternName.NODE_PEPB;
         }
     }
 
@@ -149,8 +158,8 @@ public class DefineIdCPeptideFragmentationPattern {
             int index = Integer.parseInt(ion.substring(1));
             String ionName = ion.substring(0, 1);
             check(pep, ionName, index);
-        // ion coming from a peptidebackbone linked to another if contains lep
-        // doublyCharged_pepA_b1_lepB_b2
+            // ion coming from a peptidebackbone linked to another if contains lep
+            // doublyCharged_pepA_b1_lepB_b2
         } else if (split.length == 5 && split[3].contains("lep")) {
             String lep = split[3],
                     ion = split[4],
@@ -162,8 +171,8 @@ public class DefineIdCPeptideFragmentationPattern {
                     ionPName = ionP.substring(0, 1);
             check(pep, ionPName, indexP);
             check(lep, ionName, index);
-        //doublyCharged_pepA_a3_lepB_monolink_a17
-        //singlyCharged_pepA_b5_lepB_monolink_b3
+            //doublyCharged_pepA_a3_lepB_monolink_a17
+            //singlyCharged_pepA_b5_lepB_monolink_b3
         } else if (split.length == 6 && split[3].contains("lep") && split[4].equals("monolink")) {
             isMonoLinked = true;
             String pep = split[1],
@@ -173,13 +182,37 @@ public class DefineIdCPeptideFragmentationPattern {
             check(pep, ionName, index);
         } // two ions are collapsed into one - so consider as two peptides.. 
         // i.e. singlyCharged_pepA_y1_pepB_y1_mz=175.1189 
-        else if (split.length == 5 && split[1].contains("pep") && split[2].contains("pep")) {
-
+        else if (split.length == 5 && split[1].contains("pep") && split[3].contains("pep")) {
+            String pepA = split[1],
+                    ionA = split[2],
+                    pepB = split[3],
+                    ionB = split[4];
+            int indexA = Integer.parseInt(ionA.substring(1)),
+                    indexB = Integer.parseInt(ionB.substring(1));
+            String ionAName = ionA.substring(0, 1),
+                    ionBName = ionB.substring(0, 1);
+            check(pepA, ionAName, indexA);
+            check(pepB, ionBName, indexB);
         } // this is exactly linked one..    
         //doublyCharged_pepA_a1_lepB_a9_pepB_a9_lepA_a1
         else if (split.length == 9 && (split[3].contains("lep")) && split[7].contains("lep")) {
             ions_PepA_Node++;
             ions_PepB_Node++;
+        } //singlyCharged_pepB_b1_doublyCharged_pepB_b2_mz=130.0498 
+        else if (split.length == 6 && split[1].contains("pep") && split[4].contains("pep")) {
+            String pepFirst = split[1],
+                    ionFirst = split[2],
+                    peptSecond = split[4],
+                    ionSecond = split[5];
+            Integer indexFirst = Integer.parseInt(ionFirst.substring(1)),
+                    indexSecond = Integer.parseInt(ionSecond.substring(1));
+            String ionNameFirst = ionFirst.substring(0, 1),
+                    ionNameSecond = ionSecond.substring(0, 1);
+            check(peptSecond, ionNameSecond, indexSecond);
+            check(pepFirst, ionNameFirst, indexFirst);
+            //doublyCharged_pepA_b8_lepB_monolink_b3 singlyCharged_pepA_b8_lepB_monolink_b3_mz=1161.6302 
+        } else if (split.length == 6 && split[1].contains("pep") && split[4].equals("monolink")) {
+
         }
     }
 
@@ -193,6 +226,22 @@ public class DefineIdCPeptideFragmentationPattern {
      */
     private void check(String peptide, String ionName, int ionIndex) {
         if ((peptide.equals("pepA") || peptide.equals("lepA"))
+                && (ionName.equals("x") || ionName.equals("y") || ionName.equals("z"))
+                && (ionIndex == (pepALen - linkerPositionOnPepA + 1))) {
+            ions_PepA_Node++;
+        } else if ((peptide.equals("pepB") || peptide.equals("lepB"))
+                && (ionName.equals("x") || ionName.equals("y") || ionName.equals("z"))
+                && (ionIndex == (pepBLen - linkerPositionOnPepB + 1))) {
+            ions_PepB_Node++;
+        } else if ((peptide.equals("pepA") || peptide.equals("lepA"))
+                && (ionName.equals("a") || ionName.equals("b") || ionName.equals("c"))
+                && (ionIndex == linkerPositionOnPepA)) {
+            ions_PepA_Node++;
+        } else if ((peptide.equals("pepB") || peptide.equals("lepB"))
+                && (ionName.equals("a") || ionName.equals("b") || ionName.equals("c"))
+                && (ionIndex == linkerPositionOnPepB)) {
+            ions_PepB_Node++;
+        } else if ((peptide.equals("pepA") || peptide.equals("lepA"))
                 && (((ionName.equals("x") || ionName.equals("y") || ionName.equals("z")))
                 && (ionIndex < (pepALen - linkerPositionOnPepA + 1)))) {
             ions_PepA_Right++;
@@ -214,7 +263,7 @@ public class DefineIdCPeptideFragmentationPattern {
             ions_PepB_Right++;
         } else if ((peptide.equals("pepB") || peptide.equals("lepB"))
                 && (((ionName.equals("x") || ionName.equals("y") || ionName.equals("z")))
-                && (ionIndex < (linkerPositionOnPepB)))) {
+                && (ionIndex > (linkerPositionOnPepB)))) {
             ions_PepB_Left++;
         } else if ((peptide.equals("pepB") || peptide.equals("lepB"))
                 && (((ionName.equals("a") || ionName.equals("b") || ionName.equals("c")))
@@ -224,12 +273,7 @@ public class DefineIdCPeptideFragmentationPattern {
                 && (((ionName.equals("a") || ionName.equals("b") || ionName.equals("c")))
                 && (ionIndex > linkerPositionOnPepB))) {
             ions_PepB_Right++;
-        } else if ((peptide.equals("pepA") || peptide.equals("lepA"))
-                && (ionIndex == linkerPositionOnPepA)) {
-            ions_PepA_Node++;
-        } else if ((peptide.equals("pepB") || peptide.equals("lepB"))
-                && (ionIndex == linkerPositionOnPepB)) {
-            ions_PepB_Node++;
+
         }
     }
 
