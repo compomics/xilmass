d8601f9a65eeb67c12a1ee6f4b7312d758371cd4
sulezinha <yilmazshule@gmail.com>
2015-06-27 00:06:59 +0200

    NameTargetDecoy class is improved with different results from different search engines

--- src/main/java/analyse/XPSM/NameTargetDecoy.java
+++ src/main/java/analyse/XPSM/NameTargetDecoy.java
@@ -8,6 +8,7 @@ package analyse.XPSM;
 import analyse.XPSM.prepareOutcome.AnalyzeKojak;
 import analyse.XPSM.prepareOutcome.AnalyzeOutcomes;
 import analyse.XPSM.prepareOutcome.AnalyzePLink;
+import analyse.XPSM.prepareOutcome.AnalyzePLinkValidatedResult;
 import analyse.XPSM.prepareOutcome.AnalyzeXilmass;
 import java.io.File;
 import java.io.IOException;
@@ -29,10 +30,7 @@ public class NameTargetDecoy {
      */
     public static void main(String[] args) throws IOException {
         // heavy and light labeled Xilmass outputs
-        File  lightLabeledXilmass = new File("C:/Users/Sule/Documents/PhD/XLinked/XLinkData_Freiburg/competetives/xilmass/rawFiles/dss_hcdElite_NoLabel_Both_WT_MinPeak0.txt"),
-              heavyLabeledXilmass = new File("C:/Users/Sule/Documents/PhD/XLinked/XLinkData_Freiburg/competetives/xilmass/rawFiles/dss_hcdElite_Label_Both_WT_MinPeak0.txt"),
-                
-                //                output = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_run\\combined/pLink_elite_allBestsMerged.txt"),
+        File xilmassResFile = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\xilmass/runs/dss_hcdElite_Both_Scr4_MinPeak0_rDecoy_allPeaks.txt"),
                 // contaminant files for each data set
                 psms_contaminant_elite = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\1_Cleaning\\SGRunsHCD_identification\\velos_orbitra_elite/hcd_orbi_elite_crap_10psmFDR_psms_validated.txt"),
                 psms_contaminant_qexactive = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\1_Cleaning\\SGRunsHCD_identification\\qExactive/hcd_orbi_qexactive_crap_10psmFDR_psms_validated.txt"),
@@ -40,33 +38,41 @@ public class NameTargetDecoy {
                 prediction = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\structure\\predicted_cross_linking/xwalk_prediction_uniprot2.txt"),
                 // competetive results...
                 kojakFolderElite = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\kojak\\kojak_runs\\elite"),
-                pLinkFolderElite = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_run\\combined\\elite");
-
+                pLinkFolderElite = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_run\\pLinkValidationBased\\pLink_DSS_HCD_elite_ONLYTARGET\\all_query"),
+                //pLinkCombValidatedFile = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\targetsAlso2Pfus/hcd_elite_inter_combine.spectra_targetsPfus.txt");
+                pLinkCombValidatedFile = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_run\\pLinkValidationBased\\pLink_DSS_HCD_elite_ONLYTARGET\\1.sample\\search\\hcd_elite_inter_combine/hcd_elite_inter_combine.spectra.xls");
+        
         File psms = psms_contaminant_elite,
                 pLinkFolder = pLinkFolderElite,
                 kojakFolder = kojakFolderElite;
+        boolean isElite = true,
+                isValidatedResult = false;
 
-        boolean isElite = false;
-        File output = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_run\\combined/pLink_qexactive_all.txt");
-                
-//                "C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\kojak\\kojak_runs\\combined/kojak135_qexactive_co.txt");
-//                "C:/Users/Sule/Documents/PhD/XLinked/XLinkData_Freiburg/competetives/xilmass/xilmass_elite_WT_MinPeak0.txt");
         if (!isElite) {
             psms = psms_contaminant_qexactive;
             pLinkFolder = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_run\\combined\\qexactive");
-            kojakFolder = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\kojak\\kojak_runs\\qexactive");           
-            lightLabeledXilmass = new File("C:/Users/Sule/Documents/PhD/XLinked/XLinkData_Freiburg/competetives/xilmass/rawFiles/dss_hcdQExactive_NoLabel_Both_WT_MinPeak0.txt");
-            heavyLabeledXilmass = new File("C:/Users/Sule/Documents/PhD/XLinked/XLinkData_Freiburg/competetives/xilmass/rawFiles/dss_hcdQExactive_Label_Both_WT_MinPeak0.txt");
+            kojakFolder = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\kojak\\kojak_runs\\qexactive");
+            pLinkCombValidatedFile = new File("C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\onlyTargets/hcd_qexactive_inter_combine.spectra_only_Target_QExactive.txt");
         }
 
+        File output = new File(
+                //                "C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\kojak\\kojak_runs\\combined/kojak135_qexactive_co.txt");                
+                //                "C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\pLink\\pLink_Validated_Elite_target2Pfus.txt");
+                //                "C:/Users/Sule/Documents/PhD/XLinked/XLinkData_Freiburg/competetives/xilmass/td_dss_hcdElite_Both_Scr4_MinPeak0_rDecoy_allPeaks.txt");
+                "C:\\Users\\Sule\\Documents\\PhD\\XLinked\\XLinkData_Freiburg\\competetives\\xilmass\\td_dss_hcdElite_Both_Scr4_MinPeak0_rDecoy_allPeaks.txt");
         String[] protein_names = {"Q15149", "P62158"};
-        HashMap<String, String> resultFileNameForSpectrumFile = prepare(isElite);
 
-        int analysis = 2;
+        // NOW RUN!!!
+        int analysis = 4;
+        boolean hasTraditionalFDR = true;
+        if (analysis == 3) {
+            isValidatedResult = true;
+        }
+        HashMap<String, String> resultFileNameForSpectrumFile = prepare(isElite, isValidatedResult);
         AnalyzeOutcomes o = null;
         switch (analysis) {
             case 0:
-                o = new AnalyzeXilmass(lightLabeledXilmass, heavyLabeledXilmass, output, prediction, psms_contaminant_qexactive, protein_names);
+//                o = new AnalyzeXilmass(lightLabeledXilmass, heavyLabeledXilmass, output, prediction, psms, protein_names, hasTraditionalFDR);
                 break;
             case 1:
                 o = new AnalyzeKojak(output, kojakFolder, prediction, psms, protein_names);
@@ -74,58 +80,16 @@ public class NameTargetDecoy {
             case 2:
                 o = new AnalyzePLink(pLinkFolder, output, resultFileNameForSpectrumFile, prediction, psms, protein_names);
                 break;
+            case 3:
+                o = new AnalyzePLinkValidatedResult(pLinkCombValidatedFile, output, resultFileNameForSpectrumFile, prediction, psms, protein_names);
+                break;
+            case 4:
+                o = new AnalyzeXilmass(xilmassResFile, output, prediction, psms, protein_names, hasTraditionalFDR);
+                break;
         }
         o.run();
     }
 
-//    /**
-//     * This method finds a2/b2 pairs for each cross linked peptide
-//     *
-//     * @param input
-//     * @param output
-//     * @param first_protein_index
-//     * @param second_protein_index
-//     * @param first_protein_name
-//     * @param second_protein_name
-//     * @param spectrum_name_index
-//     * @param score_index
-//     * @param hasTraditionalDecoy
-//     * @throws FileNotFoundException
-//     * @throws IOException
-//     */
-//    private void get_paired_info(File input, File output,
-//            int first_protein_index, int second_protein_index,
-//            String[] protein_names,
-//            int spectrum_name_index, int score_index, boolean hasTraditionalDecoy,
-//            int theoretical_peaks_index) throws FileNotFoundException, IOException {
-//
-//        BufferedReader br = new BufferedReader(new FileReader(input));
-//        String line = "";
-//        BufferedWriter bw = new BufferedWriter(new FileWriter(output));
-//
-//        while ((line = br.readLine()) != null) {
-//            if (!line.startsWith("Spect")) {
-//                String[] split = line.split("\t");
-//
-//                String proteinAInfo = split[first_protein_index],
-//                        proteinBInfo = split[second_protein_index],
-//                        proteinAName = proteinAInfo.substring(0, proteinAInfo.indexOf("(")),
-//                        proteinBName = proteinBInfo.substring(0, proteinBInfo.indexOf("(")),
-//                        indexProteinA = proteinAInfo.substring(proteinAInfo.indexOf("(") + 1, proteinAInfo.indexOf(")")),
-//                        indexProteinB = proteinBInfo.substring(proteinBInfo.indexOf("(") + 1, proteinBInfo.indexOf(")")),
-//                        targetType = getTargetType(proteinAName, proteinBName, protein_names, hasTraditionalDecoy),
-//                        theoreticalPeaks = split[theoretical_peaks_index];
-//                boolean has_pair_proteinA = search_pairs(theoreticalPeaks, true),
-//                        has_pair_proteinB = search_pairs(theoreticalPeaks, false);
-//
-//                bw.write(line + "\t" + indexProteinA + "\t" + indexProteinB + "\t" + targetType + "\t" + has_pair_proteinA + "\t" + has_pair_proteinB + "\n");
-//
-//            } else {
-//                bw.write(line + "\t" + "IndexProteinA" + "\t" + "IndexProteinB" + "\t" + "Target_Decoy" + "\t" + "hasPairProteinA" + "\t" + "hasPairProteinB" + "\n");
-//            }
-//        }
-//        bw.close();
-//    }
     /**
      * This method decides if there is a2/b2 pair on given theoretical peak from
      * either proteinA (isProteinA=true) or proteinB (isProteinA=false)
@@ -165,18 +129,32 @@ public class NameTargetDecoy {
      * @param isElite
      * @return
      */
-    private static HashMap<String, String> prepare(boolean isElite) {
+    private static HashMap<String, String> prepare(boolean isElite, boolean isValidatedResult) {
         HashMap<String, String> map = new HashMap<String, String>();
-        if (isElite) {
-            map.put("hcd_elite1_inter_qry.proteins.txt", "Probe2_v_mc1_top15HCD-1.mgf");
-            map.put("hcd_elite2_inter_qry.proteins.txt", "Probe2_v_mp1_top15HCD-1.mgf");
-            map.put("hcd_elite3_inter_qry.proteins.txt", "Probe2_v_x1_top15HCD-1.mgf");
-            map.put("hcd_elite4_inter_qry.proteins.txt", "Probe2_v_x1_top15HCD-precolumn-1.mgf");
+        if (!isValidatedResult) {
+            if (isElite) {
+                map.put("hcd_elite1_inter_qry.proteins.txt", "Probe2_v_mc1_top15HCD-1.mgf");
+                map.put("hcd_elite2_inter_qry.proteins.txt", "Probe2_v_mp1_top15HCD-1.mgf");
+                map.put("hcd_elite3_inter_qry.proteins.txt", "Probe2_v_x1_top15HCD-1.mgf");
+                map.put("hcd_elite4_inter_qry.proteins.txt", "Probe2_v_x1_top15HCD-precolumn-1.mgf");
+            } else {
+                map.put("hcd_qexactive1_inter_qry.proteins.txt", "Probe2_h_X1_run1.mgf");
+                map.put("hcd_qexactive2_inter_qry.proteins.txt", "Probe2_h_X1_run2.mgf");
+                map.put("hcd_qexactive3_inter_qry.proteins.txt", "Probe2_h_X2_excl3_500ms_run1.mgf");
+                map.put("hcd_qexactive4_inter_qry.proteins.txt", "Probe2_h_X2_excl3_500ms_run2.mgf");
+            }
         } else {
-            map.put("hcd_qexactive1_inter_qry.proteins.txt", "Probe2_h_X1_run1.mgf");
-            map.put("hcd_qexactive2_inter_qry.proteins.txt", "Probe2_h_X1_run2.mgf");
-            map.put("hcd_qexactive3_inter_qry.proteins.txt", "Probe2_h_X2_excl3_500ms_run1.mgf");
-            map.put("hcd_qexactive4_inter_qry.proteins.txt", "Probe2_h_X2_excl3_500ms_run2.mgf");
+            if (isElite) {
+                map.put("File2235", "Probe2_v_mc1_top15HCD-1.mgf");
+                map.put("File2239", "Probe2_v_mp1_top15HCD-1.mgf");
+                map.put("File2241", "Probe2_v_x1_top15HCD-1.mgf");
+                map.put("File2244", "Probe2_v_x1_top15HCD-precolumn-1.mgf");
+            } else {
+                map.put("File202", "Probe2_h_X1_run1.mgf");
+                map.put("File203", "Probe2_h_X1_run2.mgf");
+                map.put("File204", "Probe2_h_X2_excl3_500ms_run1.mgf");
+                map.put("File205", "Probe2_h_X2_excl3_500ms_run2.mgf");
+            }
         }
         return map;
     }
