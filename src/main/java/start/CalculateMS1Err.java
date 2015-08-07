cebbc4d9f27a985ae5639620b2a301d34baad29b
sulezinha <yilmazshule@gmail.com>
2015-08-07 17:13:34 +0200

    now, ms1Err calculation has also negative values

--- src/main/java/start/CalculateMS1Err.java
+++ src/main/java/start/CalculateMS1Err.java
@@ -14,12 +14,12 @@ public class CalculateMS1Err {
     public static double getMS1Err(boolean isPPM, double theoreticalPrecursorMass, double measuredPrecusorMass) {
         double error = 0;
         if (isPPM) {
-            double diff = Math.abs(theoreticalPrecursorMass - measuredPrecusorMass);
+            double diff = theoreticalPrecursorMass - measuredPrecusorMass;
             double ppm_error = (diff) / measuredPrecusorMass;
             ppm_error = ppm_error * 1000000;
             error = ppm_error;
         } else {
-            double mz_error = Math.abs(theoreticalPrecursorMass - measuredPrecusorMass);
+            double mz_error = theoreticalPrecursorMass - measuredPrecusorMass;
             error = mz_error;
         }
         return error;
