695e244186bf13918273101a2d3f1edff0664212
sulezinha <yilmazshule@gmail.com>
2015-08-10 13:08:04 +0200

    ms1err calculation was as explained http://genesis.ugent.be/files/costore/practicals/bioinformatics-for-proteomics/A-Answers/A-Answers.pdf
    ms1err can be negative

--- src/main/java/start/CalculateMS1Err.java
+++ src/main/java/start/CalculateMS1Err.java
@@ -6,16 +6,29 @@
 package start;
 
 /**
- *
+ * To calculate ms1error for a selected MSnSpectrum
+ * 
  * @author Sule
  */
 public class CalculateMS1Err {
 
+    /**
+     * Calculations was explained here
+     * http://genesis.ugent.be/files/costore/practicals/bioinformatics-for-proteomics/A-Answers/A-Answers.pdf (Answer 1.3e)
+     * The calculated value is not an absolute value (is either negative or positive).
+     * 
+     * 
+     * @param isPPM true: PPM based, false: Dalton based
+     * @param theoreticalPrecursorMass - Theoretical precursor mass
+     * @param measuredPrecusorMass - Observed mass
+     * @return ms1err
+     */
     public static double getMS1Err(boolean isPPM, double theoreticalPrecursorMass, double measuredPrecusorMass) {
         double error = 0;
         if (isPPM) {
-            double diff = theoreticalPrecursorMass - measuredPrecusorMass;
-            double ppm_error = (diff) / measuredPrecusorMass;
+            // This PPM calculation was from bioinformatics-for-proteomics
+            double diff = measuredPrecusorMass - theoreticalPrecursorMass;
+            double ppm_error = (diff) / theoreticalPrecursorMass;
             ppm_error = ppm_error * 1000000;
             error = ppm_error;
         } else {
