813406f50cdac2512be90d6d217fc5c179affcff
sulezinha <yilmazshule@gmail.com>
2015-09-07 20:43:29 +0200

    Xilmass analysis for FDR calculation is ready

--- src/main/java/analyse/XPSM/prepareOutcome/AnalyzeXilmass.java
+++ src/main/java/analyse/XPSM/prepareOutcome/AnalyzeXilmass.java
@@ -5,6 +5,8 @@
  */
 package analyse.XPSM.prepareOutcome;
 
+import analyse.XPSM.outcome.XilmassResult;
+import config.ConfigHolder;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -13,200 +15,204 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.HashSet;
+import org.apache.log4j.Logger;
 
 /**
  * This class merges xilmass outputs (labeled and not labeled ones)
+ *
+ * TODO: Maybe spliting interproteins and interproteins PSMs list? Not sure...
  *
  * @author Sule
  */
 public class AnalyzeXilmass extends AnalyzeOutcomes {
 
-    private File lightLabeledFile,
-            heavyLabeledFile,
-            xilmassFile,
+    private File xilmassFolder,
             output;
-    private int indAccProA = 10,
-            indAccProB = 11,
-            linkedIndexA = 16,
-            linkedIndexB = 17,
-            spectrum_title_index = 2,
-            score_index = 9;
+    private int proteinAaccession = 9,
+            spectrum_title_index = 1;
+    private double fdr;
+    private HashSet<String> contaminant_MSMS = new HashSet<String>();
+    private HashSet<XilmassResult> validatedPSMs = new HashSet<XilmassResult>();
+    private boolean isCPepFragmentKept, // to store CPeptideFragment info on the output
+            isMS1PPM; // is MS1Err calculated with PPM... true:PPM false:Da - to write unit on the table
+    private static final Logger LOGGER = Logger.getLogger(ConfigHolder.class);
 
-    HashSet<String> contaminant_MSMS = new HashSet<String>();
-
-    public AnalyzeXilmass(File lightLabeledFile, File heavyLabeledFile, File output, File prediction_file, File psms_contaminant, String[] target_names, boolean hasTraditionalDecoy) throws IOException {
+    public AnalyzeXilmass(File xilmassFolder, File output, File prediction_file, File psms_contaminant,
+            String[] target_names, double fdr, boolean isConventionalFDR, boolean isMS1PPM) throws IOException {
         super.target_names = target_names;
         super.psms_contaminant = psms_contaminant;
         super.prediction_file = prediction_file;
-        super.hasTraditionalDecoy = hasTraditionalDecoy;
-        this.lightLabeledFile = lightLabeledFile;
-        this.heavyLabeledFile = heavyLabeledFile;
+        super.isConventionalFDR = isConventionalFDR;
+        this.xilmassFolder = xilmassFolder;
+        this.fdr = fdr;
         this.output = output;
         contaminant_MSMS = getContaminant_MSMS();
-    }
-
-    public AnalyzeXilmass(File xilmassFile, File output, File prediction_file, File psms_contaminant, String[] target_names, boolean hasTraditionalDecoy) throws IOException {
-        super.target_names = target_names;
-        super.psms_contaminant = psms_contaminant;
-        super.prediction_file = prediction_file;
-        super.hasTraditionalDecoy = hasTraditionalDecoy;
-        this.xilmassFile = xilmassFile;
-        this.output = output;
-        contaminant_MSMS = getContaminant_MSMS();
+        this.isMS1PPM = isMS1PPM;
     }
 
     /**
-     * This method reads two given files (results from labelling and results
-     * from non-labelling). Then, it finds the best scores XPSMs for one
-     * spectrum.
-     *
+     * This method read each Xilmass file on the given folder. Then, it collects
+     * every PSM on these Xilmass files Followed by finding a list of PSMs with
+     * a given FDR This list of validated PSMs is written on the output file
      *
      * @throws FileNotFoundException
      * @throws IOException
      */
-    private void merge_select_top_scored() throws FileNotFoundException, IOException {
+    @Override
+    public void run() throws FileNotFoundException, IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(output));
-        HashMap<String, Double> spectrum_and_score = new HashMap<String, Double>();
-        HashMap<String, ArrayList<String>> spectrum_and_line = new HashMap<String, ArrayList<String>>();
-        if (xilmassFile != null) {
-            fillHashSets(xilmassFile, spectrum_and_score, spectrum_and_line, bw);
+        bw.write(writeTitle() + "\n");
+        HashSet<XilmassResult> psmsList = new HashSet<XilmassResult>();
+        for (File f : xilmassFolder.listFiles()) {
+            LOGGER.info(f.getName());
+            // read each file and fill hashset with all PSMs
+            HashSet<XilmassResult> tmp_list = read_and_fill(f);
+            // merge all together
+            psmsList.addAll(tmp_list);
         }
-        // read first input one and fill hashset
-        if (lightLabeledFile != null) {
-            readFile_fillHashSets(lightLabeledFile, spectrum_and_score, spectrum_and_line, false, bw, false);
-        }
-        // read second input and fill hashset -
-        if (heavyLabeledFile != null) {
-            readFile_fillHashSets(heavyLabeledFile, spectrum_and_score, spectrum_and_line, true, bw, true);
-        }
-        // now write up..
-        for (String name : spectrum_and_line.keySet()) {
-            for (String line : spectrum_and_line.get(name)) {
-                String[] split = line.split("\t");
-                String proteinAInfo = split[indAccProA],
-                        proteinBInfo = split[indAccProB],
-                        proteinAAccession = proteinAInfo.substring(0, proteinAInfo.indexOf("(")),
-                        proteinBAccession = proteinBInfo.substring(0, proteinBInfo.indexOf("(")),
-                        targetType = getTargetType(proteinAAccession, proteinBAccession, target_names),
-                        indexProteinA = proteinAInfo.substring(proteinAInfo.indexOf("(") + 1, proteinAInfo.indexOf(")")),
-                        indexProteinB = proteinBInfo.substring(proteinBInfo.indexOf("(") + 1, proteinBInfo.indexOf(")"));
-                int tmplinkedIndexA = Integer.parseInt(split[linkedIndexA]),
-                        tmplinkedIndexB = Integer.parseInt(split[linkedIndexB]),
-                        startIndexProteinA = Integer.parseInt(proteinAInfo.substring(proteinAInfo.indexOf("(") + 1, proteinAInfo.indexOf("-"))),
-                        startIndexProteinB = Integer.parseInt(proteinBInfo.substring(proteinBInfo.indexOf("(") + 1, proteinBInfo.indexOf("-"))),
-                        uniprotLinkingSiteA = startIndexProteinA + tmplinkedIndexA - 1,
-                        uniprotLinkingSiteB = startIndexProteinB + tmplinkedIndexB - 1;
-                // this retruns "Predicted" or "NotPredicted"
-                String trueCrossLinking = assetTrueLinking(proteinAAccession, proteinBAccession, uniprotLinkingSiteA, uniprotLinkingSiteB);
-                String info = "";
-                for (int index = 1; index < split.length; index++) {
-                    info += split[index] + "\t";
-                }
-                bw.write(info + indexProteinA + "\t" + indexProteinB + "\t" + targetType + "\t" + trueCrossLinking + "\n");
-            }
-        }
+        ArrayList<XilmassResult> res = new ArrayList<XilmassResult>(psmsList);
+        // sort filled list        
+        Collections.sort(res, XilmassResult.ScoreDSC);
+        // select PSMs with a given FDR value
+        validatedPSMs = getValidatedPSMs(res, fdr);
+        // write validated PSMs
+        writeOutput(validatedPSMs, bw);
         bw.close();
     }
 
     /**
-     * To merge light and heavy labeled output files
+     * This method writes every validated PSM on a given BufferedWriter
      *
-     * @param input
-     * @param spectrum_and_score
-     * @param spectrum_and_line
-     * @param isTitleWritten
+     * @param validatedPSMs
      * @param bw
-     * @param isInputLabeled
      * @throws NumberFormatException
      * @throws IOException
      */
-    private void readFile_fillHashSets(File input,
-            HashMap<String, Double> spectrum_and_score, HashMap<String, ArrayList<String>> spectrum_and_line,
-            boolean isTitleWritten, BufferedWriter bw,
-            boolean isInputLabeled) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new FileReader(input));
-        String line = "",
-                labeled = "heavy";
-        if (!isInputLabeled) {
-            labeled = "light";
-        }
-        while ((line = br.readLine()) != null) {
-            if (!line.startsWith("Spect")) {
-                String[] split = line.split("\t");
-                String specTitle = split[spectrum_title_index];
-                double tmpScore = Double.parseDouble(split[score_index]);
-                if (!contaminant_MSMS.contains(specTitle)) {
-                    if (spectrum_and_score.containsKey(specTitle)) {
-                        double storedScore = spectrum_and_score.get(specTitle);
-                        if (storedScore < tmpScore) {
-                            spectrum_and_line.remove(specTitle);
-                            spectrum_and_score.remove(specTitle);
-                            String info = line + "\t" + labeled;
-                            ArrayList<String> infos = new ArrayList<String>();
-                            infos.add(info);
-                            spectrum_and_line.put(specTitle, infos);
-                            spectrum_and_score.put(specTitle, tmpScore);
-                        } else if (storedScore == tmpScore) {
-                            String info = line + "\t" + labeled;
-                            spectrum_and_line.get(specTitle).add(info);
-                        }
-                    } else if (!spectrum_and_score.containsKey(specTitle)) {
-                        String info = line + "\t" + labeled;
-                        ArrayList<String> infos = new ArrayList<String>();
-                        infos.add(info);
-                        spectrum_and_line.put(specTitle, infos);
-                        spectrum_and_score.put(specTitle, tmpScore);
-                    }
-                }
-            } else if (!isTitleWritten) {
-                bw.write(line.substring(line.indexOf("SpectrumFile")) +"\t" + "IndexProteinA" + "\t" + "IndexProteinB" + "\t"
-                        + "Target_Decoy" + "\t" + "Predicted" + "\t" + "Euclidean_distance beta(A)" + "\t" + "Euclidean_distance alpha(A)" + "\n");
-            }
+    private void writeOutput(HashSet<XilmassResult> validatedPSMs, BufferedWriter bw) throws NumberFormatException, IOException {
+        // now write up..
+        for (XilmassResult validatedPSM : validatedPSMs) {
+            bw.write(validatedPSM.toPrint());
+            bw.newLine();
         }
     }
 
-    @Override
-    public void run() throws FileNotFoundException, IOException {
-        merge_select_top_scored();
-    }
-
-    private void fillHashSets(File xilmassFile, HashMap<String, Double> spectrum_and_score, HashMap<String, ArrayList<String>> spectrum_and_line, BufferedWriter bw) throws FileNotFoundException, IOException {
-        BufferedReader br = new BufferedReader(new FileReader(xilmassFile));
+    /**
+     * This method reads a given file and select every PSM if they are matched
+     * to contaminants
+     *
+     * @param f xilmass results files
+     * @return a list of XilmassResults objects from a given file
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    private HashSet<XilmassResult> read_and_fill(File f) throws FileNotFoundException, IOException {
+        HashSet<XilmassResult> res = new HashSet<XilmassResult>();
+        BufferedReader br = new BufferedReader(new FileReader(f));
         String line = "";
-        boolean isTitleWritten = false;
         while ((line = br.readLine()) != null) {
             if (!line.startsWith("Spect")) {
                 String[] split = line.split("\t");
-                String specTitle = split[spectrum_title_index];
-                double tmpScore = Double.parseDouble(split[score_index]);
-                if (!contaminant_MSMS.contains(specTitle)) {
-                    if (spectrum_and_score.containsKey(specTitle)) {
-                        double storedScore = spectrum_and_score.get(specTitle);
-                        if (storedScore < tmpScore) {
-                            spectrum_and_line.remove(specTitle);
-                            spectrum_and_score.remove(specTitle);
-                            ArrayList<String> infos = new ArrayList<String>();
-                            infos.add(line);
-                            spectrum_and_line.put(specTitle, infos);
-                            spectrum_and_score.put(specTitle, tmpScore);
-                        } else if (storedScore == tmpScore) {
-                            spectrum_and_line.get(specTitle).add(line);
-                        }
-                    } else if (!spectrum_and_score.containsKey(specTitle)) {
-                        ArrayList<String> infos = new ArrayList<String>();
-                        infos.add(line);
-                        spectrum_and_line.put(specTitle, infos);
-                        spectrum_and_score.put(specTitle, tmpScore);
-                    }
+                String specTitle = split[spectrum_title_index],
+                        proteinA = split[proteinAaccession];
+                if (!contaminant_MSMS.contains(specTitle) && !proteinA.contains("contaminant")) {
+                    res.add(new XilmassResult(line, isCPepFragmentKept));
                 }
-            } else if (!isTitleWritten) {
-                bw.write(line.substring(line.indexOf("SpectrumFile")) +  "\t" + "IndexProteinA" + "\t" + "IndexProteinB" + "\t"
-                        + "Target_Decoy" + "\t" + "Predicted" + "\t" + "Euclidean_distance beta(A)" + "\t" + "Euclidean_distance alpha(A)" + "\n");
-                isTitleWritten = true;
             }
         }
+        return res;
+    }
+
+    /**
+     * To write a title on the output with the validated PSMs
+     *
+     * @return the title
+     *
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    private String writeTitle() throws FileNotFoundException, IOException {
+        String ms1Err = "MS1Err(PPM)",
+                absMS1Err = "AbsMS1Err(PPM)";
+        if (!isMS1PPM) {
+            ms1Err = "MS1Err(Da)";
+            absMS1Err = "AbsMS1Err(Da)";
+        }
+        String title = "SpectrumFile" + "\t" + "SpectrumTitle" + "\t"
+                + "ObservedMass(Da)" + "\t" + "PrecursorCharge" + "\t" + "RetentionTime(Seconds)" + "\t" + "ScanNr" + "\t"
+                + ms1Err + "\t" + absMS1Err + "\t"
+                + "PeptideA" + "\t" + "ProteinA" + "\t" + "ModA" + "\t"
+                + "PeptideB" + "\t" + "ProteinB" + "\t" + "ModB" + "\t"
+                + "LinkingType" + "\t"
+                + "LinkPeptideA" + "\t" + "LinkPeptideB" + "\t" + "LinkProteinA" + "\t" + "LinkProteinB" + "\t"
+                + "ScoringFunctionName" + "\t" + "Score" + "\t"
+                + "MatchedPeakList" + "\t" + "TheoMatchedPeakList" + "\t"
+                + "CPeptidePattern" + "\t"
+                + "lnNumSp" + "\t"
+                + "TargetDecoy" + "\t"
+                + "LinkerLabeling" + "\t"
+                + "Predicted" + "\t" + "EuclideanDistance(Carbon-betas-A)" + "\t" + "EuclideanDistance (Carbon alphas-A)";
+        return title;
+    }
+
+    /**
+     * This method selects the list of validated PSMs
+     *
+     * @param res a list of every PSM result from Xilmass
+     * @param fdr is given FDR value to select PSMs
+     * @return list of validated XilmassResult objects
+     *
+     * @throws IOException
+     */
+    private HashSet<XilmassResult> getValidatedPSMs(ArrayList<XilmassResult> res, double fdr) throws IOException {
+        HashSet<XilmassResult> tmpValidatedPSMlist = new HashSet<XilmassResult>();
+        double tmp_fdr = 0.00;
+        int targets = 0,
+                full_decoys = 0,
+                half_decoys = 0;
+        for (int i = 0; i < res.size(); i++) {
+            XilmassResult r = res.get(i);
+            // give a name now...
+            String proteinA = r.getProteinA(),
+                    proteinB = r.getProteinB();
+            String td = getTargetDecoy(proteinA, proteinB);
+            if (td.equals("TD")) {
+                half_decoys++;
+            } else if (td.equals("DD")) {
+                full_decoys++;
+            } else {
+                targets++;
+            }
+            // set target/decoy name on r..
+            r.setTarget_decoy(td);
+            // means any decoy divided by all target..
+            if (isConventionalFDR && (half_decoys > 0 || full_decoys > 0)) {
+                tmp_fdr = (double) (full_decoys + half_decoys) / (double) targets;
+                // means pLink based calculation..    
+            } else if (!isConventionalFDR && (half_decoys > 0 || full_decoys > 0)) {
+                tmp_fdr = (double) (half_decoys - full_decoys) / (double) targets;
+            }
+            // set true cross linking info 
+            String proteinAAccession = proteinA.substring(0, proteinA.indexOf("(")),
+                    proteinBAccession = proteinB.substring(0, proteinB.indexOf("("));
+            String trueCrossLinking = assetTrueLinking(proteinAAccession, proteinBAccession, r.getLinkProteinA(), r.getLinkProteinB());
+            // set a true cross-linking info
+            r.setTrueCrossLinking(trueCrossLinking);
+            // if the current FDR is smaller than given FDR..  select these PSMs into a lsit
+            if (fdr >= tmp_fdr && td.equals("TT")) {
+                tmpValidatedPSMlist.add(r);
+            }
+        }
+        return tmpValidatedPSMlist;
+    }
+
+    public HashSet<XilmassResult> getValidatedPSMs() {
+        return validatedPSMs;
+    }
+
+    public void setValidatedPSMs(HashSet<XilmassResult> validatedPSMs) {
+        this.validatedPSMs = validatedPSMs;
     }
 
 }
