5f26fa162edd86201bfb37389789ac7d293bac48
Genet Abay <genetabay.shiferaw@vib-ugent.be>
2018-05-16 15:18:21 +0200

    version updated

--- src/main/java/crossLinker/type/BMPS.java
+++ src/main/java/crossLinker/type/BMPS.java
@@ -6,13 +6,15 @@ import crossLinker.CrossLinkerName;
 import crossLinker.CrossLinkerType;
 
 /**
- *
+ *This class defines properties of cross linker BMPS
  * @author Genet
  */
 public class BMPS extends CrossLinker {
     
     
-    
+    /**
+     * Initializing the property of BMPS
+     */
      public BMPS() {
         this.name = CrossLinkerName.BMPS;
         this.type = CrossLinkerType.AMINE_TO_SULFHYDRYL;  
@@ -22,7 +24,10 @@ public class BMPS extends CrossLinker {
         super.massShift_Type2 = moleculeMass - (2 * Atom.H.getMonoisotopicMass()+ Atom.O.getMonoisotopicMass());
     }
 
-   
+   /**
+    * Calculates mass shift based on the formula 
+    * @param isLabeled 
+    */
     @Override
     protected void calculateMassShifts(boolean isLabeled) {
         
@@ -31,8 +36,8 @@ public class BMPS extends CrossLinker {
             double moleculeMass = (7 * Atom.C.getMonoisotopicMass()) + Atom.N.getMonoisotopicMass () + (4 * Atom.O.getMonoisotopicMass()) + (7 * Atom.H.getMonoisotopicMass());
             super.massShift_Type0 = moleculeMass ;
             super.massShift_Type2 = moleculeMass - (2 * Atom.H.getMonoisotopicMass()+ Atom.O.getMonoisotopicMass());
-        } else {
-            throw new UnsupportedOperationException("Labling should be false, no labeling feature provided."); 
+        } else {//No labeling information given and hence it doesn't support
+            throw new UnsupportedOperationException("Labeling should be false, no labeling feature provided."); 
         }
     }
 }
